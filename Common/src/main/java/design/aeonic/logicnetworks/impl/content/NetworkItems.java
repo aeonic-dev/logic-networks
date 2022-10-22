8d18acf8f32d0f3df0e58b8c93f035f6d4a8d266
aeonic <io@aeonic.design>
2022-10-22 17:04:16 -0500

    Logic Core item

--- Common/src/main/java/design/aeonic/logicnetworks/impl/content/NetworkItems.java
+++ Common/src/main/java/design/aeonic/logicnetworks/impl/content/NetworkItems.java
@@ -10,6 +10,7 @@ public final class NetworkItems {
     public static final BlockItem NETWORK_ANCHOR = new BlockItem(NetworkBlocks.NETWORK_ANCHOR, new BlockItem.Properties());
 
     public static final LinkingCardItem LINKING_CARD = new LinkingCardItem(new Item.Properties().stacksTo(1));
+    public static final Item LOGIC_CORE = new Item(new Item.Properties().stacksTo(64));
 
     public static void register(Registrar<Item> registrar) {
         registrar.accept("network_controller", NETWORK_CONTROLLER);
